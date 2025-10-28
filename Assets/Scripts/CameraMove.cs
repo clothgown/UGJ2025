c9bdc5b600c20846facc804409574f9133b430f6
WELLAPC <1843560248@qq.com>
2025-10-28 22:39:08 +0800

    1

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -23,7 +23,7 @@ public class CameraMove : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
+
         }
         else
         {
