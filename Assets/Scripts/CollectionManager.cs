74961f5ec4a824a37c97163f040da508913f355d
WELLA <1843560248@qq.com>
2025-10-31 23:22:36 +0800

    1

--- Assets/Scripts/CollectionManager.cs
+++ Assets/Scripts/CollectionManager.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollectionManager : MonoBehaviour
 {
     public static CollectionManager instance;
     public int coins=0;
     public int thisLevelGetCoins;
+
+    public bool isEntered1_5;
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject); 
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
         {
@@ -22,7 +26,20 @@ public class CollectionManager : MonoBehaviour
     }
 
     // Start is called before the first frame update
-
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // 检查当前场景名称是否为"1-5"
+        if (scene.name == "1-5 1 laukinwang")
+        {
+            isEntered1_5 = true;
+            Debug.Log("已进入场景1-5，isEntered1_5设置为true");
+        }
+        else
+        {
+            // 可选：如果不在场景1-5时重置标志
+            // isEntered1_5 = false;
+        }
+    }
 
     // Update is called once per frame
     void Update()
